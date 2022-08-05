6489248d81b41e79ba3b3120b43908b5c3d11469
Ajay-2007 <iit2017040@iiita.ac.in>
2022-08-05 15:31:06 +0530

    intro to jest

--- job-search/src/playground.js
+++ job-search/src/playground.js
@@ -3,21 +3,30 @@
 
 // console.log([fruits, vegetables]);
 // console.log([...fruits, ...vegetables]);
-const developer = {
-  salary: 100000,
-  experience: 4.5,
-  techStack: ["Vue", "HTML", "CSS"],
-  lookingForWork: true,
-  doubleSalary() {
-    this.salary = this.salary * 2;
-    this.lookingForWork = false;
-  },
-};
+// const developer = {
+//   salary: 100000,
+//   experience: 4.5,
+//   techStack: ["Vue", "HTML", "CSS"],
+//   lookingForWork: true,
+//   doubleSalary() {
+//     this.salary = this.salary * 2;
+//     this.lookingForWork = false;
+//   },
+// };
 
-console.log(developer.salary);
-console.log(developer.lookingForWork);
+// console.log(developer.salary);
+// console.log(developer.lookingForWork);
 
-developer.doubleSalary();
+// developer.doubleSalary();
 
-console.log(developer.salary);
-console.log(developer.lookingForWork);
+// console.log(developer.salary);
+// console.log(developer.lookingForWork);
+// export const evenOrOdd = (number) => {
+//   if (number % 2 === 0) {
+//     return "Even";
+//   } else {
+//     return "Odd";
+//   }
+// };
+
+// export const multiply = (num1, num2) => num1 * num2;
